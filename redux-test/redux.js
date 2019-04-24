c1b8a45b03a1072ce6e0682a277a8c87355a0c65
Hamid Dezhkam <hamid.dezhkam@gmail.com>
2019-04-24 12:54:32 +1000

    04242019T1254

--- redux-test/redux.js
+++ redux-test/redux.js
@@ -1,18 +1,167 @@
+
+//////////////////////////// without action generator
+
+// const { createStore } = require('redux');
+
+// const store = createStore((state = {count:0}, action) => {
+    
+//     switch (action.type) {
+//         case 'INCREMENT':
+//             return {
+//                 count: state.count + 1
+//             }
+//         case 'DECREMENT':
+//             return {
+//                 count: state.count - 1
+//             }
+//         case 'SET':
+//             return {
+//                 count: action.count
+//             }
+//         case 'RESET':
+//             return {
+//                 count: 0
+//             }
+//         default:
+//             return state;
+//     }
+
+//     return state;
+// });
+
+// store.subscribe(() => {
+//     console.log(store.getState());
+// });
+
+// store.dispatch({
+//     type: 'INCREMENT'
+// });
+
+// store.dispatch({
+//     type: 'INCREMENT'
+// });
+
+// store.dispatch({
+//     type: 'DECREMENT'
+// });
+
+// store.dispatch({
+//     type: 'SET',
+//     count: 101
+// });
+
+// store.dispatch({
+//     type: 'RESET'
+// });
+
+////////////////////////////// with action generator
+
+// const { createStore } = require('redux');
+
+// const store = createStore((state = {count:0}, action) => {
+    
+//     switch (action.type) {
+//         case 'INCREMENT':
+            
+//             return {
+//                 count: state.count + action.incrementBy
+//             }
+//         case 'DECREMENT':
+//             return {
+//                 count: state.count - 1
+//             }
+//         case 'SET':
+//             return {
+//                 count: action.count
+//             }
+//         case 'RESET':
+//             return {
+//                 count: 0
+//             }
+//         default:
+//             return state;
+//     }
+
+//     return state;
+// });
+
+// store.subscribe(() => {
+//     console.log(store.getState());
+// });
+
+// const incrementBy = (uns = {incrementBy:1}) => ({
+//     type: 'INCREMENT',
+//     incrementBy: typeof uns.incrementBy === 'number' ? uns.incrementBy : 1
+// });
+
+// store.dispatch(incrementBy({incrementBy: 5}));
+
+// store.dispatch(incrementBy());
+
+// store.dispatch({
+//     type: 'DECREMENT'
+// });
+
+// store.dispatch({
+//     type: 'SET',
+//     count: 101
+// });
+
+// store.dispatch({
+//     type: 'RESET'
+// });
+////////////////////////////// with action generator & REDUCER
 const { createStore } = require('redux');
 
-const store = createStore((state = {count:0}, action) => {
-    if (action.type === 'INCREMENT') {
-        return {
-            count: state.count + 1
-        }
-    } else {
-        return state;
+const reducer = (state = {count:0}, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + action.incrementBy
+            }
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            }
+        case 'SET':
+            return {
+                count: action.count
+            }
+        case 'RESET':
+            return {
+                count: 0
+            }
+        default:
+            return state;
     }
-    return state;
+};
+
+const store = createStore(reducer);
+
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+const incrementBy = (uns = {incrementBy:1}) => ({
+    type: 'INCREMENT',
+    incrementBy: typeof uns.incrementBy === 'number' ? uns.incrementBy : 1
+});
+
+store.dispatch(incrementBy({incrementBy: 5}));
+
+store.dispatch(incrementBy());
+
+store.dispatch({
+    type: 'DECREMENT'
 });
 
 store.dispatch({
-    type: 'INCREMENT'
+    type: 'SET',
+    count: 101
 });
 
-console.log(store.getState());
+store.dispatch({
+    type: 'RESET'
+});
+
+
