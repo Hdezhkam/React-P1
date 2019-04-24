0346ca7c7a762283a00b7330c50eb25b0b563cd9
Hamid Dezhkam <hamid.dezhkam@gmail.com>
2019-04-24 11:32:55 +1000

    04242019T1132

---
+++ redux-test/redux.js
@@ -0,0 +1,18 @@
+const { createStore } = require('redux');
+
+const store = createStore((state = {count:0}, action) => {
+    if (action.type === 'INCREMENT') {
+        return {
+            count: state.count + 1
+        }
+    } else {
+        return state;
+    }
+    return state;
+});
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+console.log(store.getState());
