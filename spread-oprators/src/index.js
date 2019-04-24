99f0d6f7d589d7e8c9ad378b6bea5da45e20d41c
Hamid Dezhkam <hamid.dezhkam@gmail.com>
2019-04-24 14:58:16 +1000

    04242019T1458

---
+++ spread-oprators/src/index.js
@@ -0,0 +1,20 @@
+// const arr = [1,2,3];
+
+// const arr2 = [...arr,4,5,6];
+
+// console.log([...arr,4,5,6]);
+// console.log(arr);
+
+const obj = {
+    firstName: 'Jonah',
+    lastName: 'Gh'
+};
+
+const obj2 = {
+    ...obj,
+    lastName: 'Toplearn',
+    age: 27
+};
+
+console.log(obj);
+console.log(obj2);
