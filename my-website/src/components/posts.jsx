67359de9ed2e55c66f4fe2ae6d1b996d52427dfb
Hamid Dezhkam <hamid.dezhkam@gmail.com>
2019-04-25 16:51:09 +1000

    04252019T1651

--- my-website/src/components/posts.jsx
+++ my-website/src/components/posts.jsx
@@ -13,23 +13,26 @@ class Posts extends Component {
         this.setState({ posts: posts });
     }
     render() {
+        const {posts}= this.state;
         return (
-            <div className="container-fluid">
+            <React.Fragment>
+                {posts.map(post => (
+                <div className="container-fluid">
                 <div className="card shadow-lg bg-light m-2">
                     <article className="p-3">
                         <div className="card-header">
                             <h3 className="card-title">
-                                <a href="#">Test</a>
+                                <a href="#">{post.postTitle}</a>
                             </h3>
                             <span className="card-subtitle">
                                 <span className="fa fa-calendar m-2"/>
-                                20 Oct
+                                {post.postDate}
                             </span>
-                            <img className="card-img" src="https://via.placeholder.com/700X300" alt="" />
+                            <img className="card-img" src="post.postImageUrl" alt="" />
                         </div>
                         <div className="card-body">
                             <p className="card-text">
-                                Consectetur commodo consequat velit ut laborum eu nulla. Dolor adipisicing elit sit do nulla officia incididunt ipsum dolore cillum nostrud deserunt fugiat enim. Elit Lorem esse quis velit est. In id occaecat id enim nisi et fugiat. Consequat id dolor nulla laborum pariatur non officia id cupidatat irure ea veniam fugiat dolore. Pariatur tempor laboris qui do consequat. Officia quis ut id cupidatat dolor laboris labore.
+                                {post.postContent}
                             </p>
                         </div>
                         <div className="card-footer">
@@ -38,15 +41,14 @@ class Posts extends Component {
                                     <span className="fa fa-link m-1"/>
                                     Labels:
                                 </li>
-                                <li className="list-inline-item"><a href="#">Test</a></li>
-                                <li className="list-inline-item"><a href="#">Test</a></li>
-                                <li className="list-inline-item"><a href="#">Test</a></li>
-                                <li className="list-inline-item"><a href="#">Test</a></li>
+                                <li className="list-inline-item"><a href="#">{post.postTags}</a></li>
                             </ul>
                         </div>
                     </article>
                 </div>
             </div>
+            ))}
+            </React.Fragment>
         );
     }
 }
