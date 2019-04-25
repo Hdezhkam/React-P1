a36b4a5b2bbbd887c6215c6c8df868486153c169
Hamid Dezhkam <hamid.dezhkam@gmail.com>
2019-04-25 17:39:24 +1000

    04252019T1739

--- my-website/src/components/posts.jsx
+++ my-website/src/components/posts.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import getPosts from './../services/fakePosts';
+import Like from './../like';
+
 
 class Posts extends Component {
     state = {
-        posts : [],
+        posts: [],
         currentPage: 1,
         pageSize: 5
     }
@@ -13,41 +15,42 @@ class Posts extends Component {
         this.setState({ posts: posts });
     }
     render() {
-        const {posts}= this.state;
+        const { posts } = this.state;
         return (
             <React.Fragment>
                 {posts.map(post => (
-                <div className="container-fluid">
-                <div className="card shadow-lg bg-light m-2">
-                    <article className="p-3">
-                        <div className="card-header">
-                            <h3 className="card-title">
-                                <a href="#">{post.postTitle}</a>
-                            </h3>
-                            <span className="card-subtitle">
-                                <span className="fa fa-calendar m-2"/>
-                                {post.postDate}
-                            </span>
-                            <img className="card-img" src="post.postImageUrl" alt="" />
+                    <div className="container-fluid" key={post.id}>
+                        <div className="card shadow-lg bg-light m-2">
+                            <article className="p-3">
+                                <div className="card-header">
+                                    <h3 className="card-title">
+                                        <a href="#">{post.postTitle}</a>
+                                    </h3>
+                                    <span className="card-subtitle">
+                                        <span className="fa fa-calendar m-2" />
+                                        {post.postDate}
+                                    </span>
+                                    <img className="card-img" src="post.postImageUrl" alt="" />
+                                </div>
+                                <div className="card-body">
+                                    <p className="card-text">
+                                        {post.postContent}
+                                    </p>
+                                </div>
+                                <div className="card-footer">
+                                    <ul className="list-inline float-left">
+                                        <li className="list-inline-item">
+                                            <span className="fa fa-link m-1" />
+                                            Labels:
+                                        </li>
+                                        <li className="list-inline-item"><a href="#">{post.postTags}</a></li>
+                                    </ul>
+                                    <Like post={post} />
+                                </div>
+                            </article>
                         </div>
-                        <div className="card-body">
-                            <p className="card-text">
-                                {post.postContent}
-                            </p>
-                        </div>
-                        <div className="card-footer">
-                            <ul className="list-inline float-left">
-                                <li className="list-inline-item">
-                                    <span className="fa fa-link m-1"/>
-                                    Labels:
-                                </li>
-                                <li className="list-inline-item"><a href="#">{post.postTags}</a></li>
-                            </ul>
-                        </div>
-                    </article>
-                </div>
-            </div>
-            ))}
+                    </div>
+                ))}
             </React.Fragment>
         );
     }
